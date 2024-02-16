19492c5528f8906342ca74f1cb0525cf5341f11f
unknown <dpaiva266@hotmail.com>
2024-02-16 11:15:00 -0300

    *Adicionado mais uma teg na class produto

--- PrimeiraClass.java
+++ PrimeiraClass.java
@@ -6,18 +6,21 @@ public class PrimeiraClass {
         Cliente cliente = new Cliente();
         Fucionario fucionario = new Fucionario();
         ItenVenda itenVenda = new ItenVenda();
+        Produto produto = new Produto();
 
         cliente.setCodigo(123);
         cliente.setEndereco("Rua teste");
         fucionario.setNome("Daniel");
         fucionario.setEndereco("Rua dois");
         itenVenda.setQuantidade(5);
+        produto.setNomeProduto("Geladeira");
 
         System.out.println(cliente.getCodigo());
         System.out.println(cliente.getEndereco());
         System.out.println(fucionario.getNome());
         System.out.println(fucionario.getEndereco());
         System.out.println(itenVenda.getQuantidade());
+        System.out.println(produto.getNomeProduto());
 
     }
 }
--- Produto.java
+++ Produto.java
@@ -1,5 +1,13 @@
 public class Produto {
 
+    private String nomeProduto;
 
+    public String getNomeProduto() {
+        return this.nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
 
 }
