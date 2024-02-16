432081800a4a5f609c9def3d128379006496a820
unknown <dpaiva266@hotmail.com>
2024-02-16 10:59:45 -0300

    *Adicionado a teg iten venda

--- PrimeiraClass.java
+++ PrimeiraClass.java
@@ -5,16 +5,19 @@ public class PrimeiraClass {
 
         Cliente cliente = new Cliente();
         Fucionario fucionario = new Fucionario();
+        ItenVenda itenVenda = new ItenVenda();
 
         cliente.setCodigo(123);
         cliente.setEndereco("Rua teste");
         fucionario.setNome("Daniel");
         fucionario.setEndereco("Rua dois");
+        itenVenda.setQuantidade(5);
 
         System.out.println(cliente.getCodigo());
         System.out.println(cliente.getEndereco());
         System.out.println(fucionario.getNome());
         System.out.println(fucionario.getEndereco());
+        System.out.println(itenVenda.getQuantidade());
 
     }
 }
